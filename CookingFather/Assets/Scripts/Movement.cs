3abe0667c5e3e08cb260c75eaf56b77e42bd2730
bigman79 <64438880+bigman79@users.noreply.github.com>
2024-09-07 18:55:52 -0700

    Revising movement
    also added camera script however have not added it's contents yet

--- CookingFather/Assets/Scripts/Movement.cs
+++ CookingFather/Assets/Scripts/Movement.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    
+    
+    
     private Rigidbody rb;
     
     private float speed = 10f; 
     private float stopfactor = 0.99f;
     private float increasefactor = 1f;
-    
     private bool isGrounded;
     void Start()
     {
@@ -20,22 +23,30 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        turn();
+        
         walk();
         run();
+        jump();
+        crouch();
+        
+    }
+    
+    private void crouch()
+    {
+        
+    }
+
+    private void jump()
+    {
+        
     }
 
     private void run()
     {
+        //change look speed depending on running
         
     }
-
-    private void turn()
-    {
-        
-        
-    }
-
+    
     private void walk()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -45,11 +56,17 @@ public class Movement : MonoBehaviour
         
         Vector3 horizontalMovement = transform.right * horizontal;
         Vector3 verticalMovement = transform.forward * vertical;
-        
+        Vector3 movement = horizontalMovement + verticalMovement;
         Vector3 _velocity = (horizontalMovement + verticalMovement).normalized * (speed * increasefactor);
         Vector3 _stop = new Vector3(rb.velocity.x * stopfactor, rb.velocity.y, rb.velocity.z * stopfactor);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        }
+
         //Acceleration
-        increasefactor = Mathf.SmoothDamp(7,1,ref increasefactor,0.1f);
+        increasefactor = Mathf.SmoothDamp(5,1,ref increasefactor,0.1f);
         rb.velocity += _velocity * Time.fixedDeltaTime;
         
         //checks if player stopped moving
@@ -60,7 +77,6 @@ public class Movement : MonoBehaviour
                 rb.velocity = _stop * Time.fixedDeltaTime;
             }
         }
-        
         
         
         //speed cap
