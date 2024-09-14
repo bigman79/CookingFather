fd19620246f98e1ca2732cb38b855fd068406718
bigman79 <64438880+bigman79@users.noreply.github.com>
2024-09-14 11:09:03 -0700

    added Input Manager to movement

--- CookingFather/Assets/Scripts/Movement.cs
+++ CookingFather/Assets/Scripts/Movement.cs
@@ -1,21 +1,20 @@
+using System;
+using System.Collections;
 
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public Rigidbody rb;
     private float _walkspeed = 7f;
     
-    private Vector3 _inputs;
-    
     private List<Vector3> Inputs;
-=======
-<<<<<<< Updated upstream
-=======
+
+
     private List<Vector3> _inlist;
->>>>>>> Desktop
+
     
     private Rigidbody _rb;
 
@@ -24,83 +23,18 @@ public class Movement : MonoBehaviour
     
     private Vector3 _inputs;
     
->>>>>>> Stashed changes
+
     
     [SerializeField] private bool isRunning;
     [SerializeField] private bool isGrounded;
-    
+
     void Start()
     {
-<<<<<<< Updated upstream
+
         rb = GetComponent<Rigidbody>();
         Inputs = new List<Vector3>();
-
     }
 
-    void Update()
-    {
-        Inputs.Add(_inputs);
-        
-    }
-    
-    void FixedUpdate()
-    {
-        listinnit();
-        Walk();
-    }
-
-    private void listinnit()
-    {
-        float xraw = Input.GetAxisRaw("Horizontal");
-        float zraw = Input.GetAxisRaw("Vertical");
-        
-        _inputs = new Vector3(xraw, 0, zraw);
-        if(Inputs.Count == 10)
-        {
-            Vector3 first = Inputs[Inputs.Count];
-            Vector3 last = Inputs[Inputs.Count - 1];
-            if (first == -last)
-            {
-                //turned 180
-                print("turned 180");
-            }
-            if(last == Vector3.zero && first != Vector3.zero)
-            {
-               //moved
-               print("moved");
-            }
-        }
-        
-        // Remove the first element in the list
-        if (Inputs.Count > 10)
-        {
-            Inputs.RemoveAt(0);
-        }
-        
-    }
-
-    private void Walk()
-    {
-<<<<<<< HEAD
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        
-        Vector3 move = (transform.right * x + transform.forward * z) * _walkspeed;
-        move *= Time.fixedDeltaTime;
-        rb.MovePosition(transform.position + move);
-=======
-        isGrounded = false;
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        isGrounded = true;
-=======
-        _inlist = new List<Vector3>();
-        _rb = GetComponent<Rigidbody>();
->>>>>>> Desktop
-    }
-    
     void Update()
     {
         Listinnit();
@@ -137,7 +71,6 @@ public class Movement : MonoBehaviour
         {
             Walk(x,z);
         }
->>>>>>> Stashed changes
     }
 
     private void Run(float x, float z)
