5dba57502da3e48bf2f45f0e2c0d7f77a1b4adf4
bigman79 <64438880+bigman79@users.noreply.github.com>
2024-09-14 10:59:53 -0700

    added Input Manager to movement

--- CookingFather/Assets/Scripts/Movement.cs
+++ CookingFather/Assets/Scripts/Movement.cs
@@ -1,12 +1,25 @@
-using System;
-using System.Collections;
+
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    private List<Vector3> _inlist;
     
+    private Rigidbody _rb;
+
+    [SerializeField] private float walkspeed;
+    [SerializeField] private float runspeed;
+    
+    private Vector3 _inputs;
+    
+>>>>>>> Stashed changes
+    
+    [SerializeField] private bool isRunning;
+    [SerializeField] private bool isGrounded;
     
     
     private Rigidbody rb;
@@ -17,6 +30,7 @@ public class Movement : MonoBehaviour
     private bool isGrounded;
     void Start()
     {
+<<<<<<< Updated upstream
         rb = GetComponent<Rigidbody>();
     }
 
@@ -95,5 +109,84 @@ public class Movement : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         isGrounded = true;
+=======
+        _inlist = new List<Vector3>();
+        _rb = GetComponent<Rigidbody>();
     }
+    
+    void Update()
+    {
+        Listinnit();
+    }
+    private void Listinnit()
+    {
+        float xraw = Input.GetAxisRaw("Horizontal");
+        float zraw = Input.GetAxisRaw("Vertical");
+        _inlist.Add(new Vector3(xraw,0,zraw));
+        _inlist.Capacity = 3;
+        if (_inlist.Count == _inlist.Capacity)
+        {
+            _inlist.RemoveAt(0);
+            
+        }
+    }
+
+    void FixedUpdate()
+    {
+        InputManager();
+
+    }
+
+    private void InputManager()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        isRunning = Input.GetButton("Run");
+        if (isRunning && isGrounded)
+        {
+            Run(x,z);
+        }
+        else if (!isRunning && isGrounded)
+        {
+            Walk(x,z);
+        }
+>>>>>>> Stashed changes
+    }
+
+    private void Run(float x, float z)
+    {
+        Vector3 input = new Vector3(x, 0, z);
+
+        float Runspeed(float horizontal)
+        {
+            return 0;
+        }
+
+        Vector3 rmove = transform.forward * Runspeed(x);
+        rmove *= Time.fixedDeltaTime;
+        _rb.MovePosition(transform.position + rmove);
+        Quaternion rotation = Quaternion.LookRotation(input.normalized);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * 10);
+        
+    }
+
+    private void Walk(float x, float z)
+    {
+        
+        Vector3 move = (transform.right * x + transform.forward * z) * walkspeed;
+        move *= Time.fixedDeltaTime;
+        _rb.MovePosition(transform.position + move);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        isRunning = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+    
+    
 }
